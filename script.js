17a834f096b99a84b4fc7df286bba34f6b59e631
Zakaria Muse <zaknvf@Zakarias-MacBook-Air.local>
2023-10-10 10:55:07 -0500

    updated by adding stretch goal

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ $( document ).ready(function() {
     console.log("test"); 
     $("th").css({"font-size": "20px", "font-style": "italic"});
     $("td:nth-child(odd)").css("backgroundColor","lightblue");
-    $("td:nth-child(even)").css("backgroundColor","tomato");
+    
+
+    checkCost();
+    // nth-child cannot equal zero, var = i has to be more than 0
+    function checkCost(){
+        for(var i = 1; i <= $("tr").length; i++){
+            var item = $("tr:nth-child(" + i +") td:nth-child(2)");
+            console.log("this is item", item);
+            var cost = parseFloat(item.text());
+
+            if(cost > 10){
+                item.css("background-color", "tomato");
+            }
+        }
+    }
 
 });
